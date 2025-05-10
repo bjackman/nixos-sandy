1fcaf32fb526220284f507ed3a2fd793b9a94e10
Brendan Jackman <bhenryj0117@gmail.com>
2025-05-10 12:35:05 +0200

    Initial attempt at separate flake for sandy
    Created a new flake and imported my central nixos config flake.
    
    To try and minimize changes I also manually locked the nixpkgs
    version to the same version used by that flake. There are 2 nixpkgs
    inputs in the lock, I assume one is the version used by this flake
    and one is the transitive input i.e. the other flake's inputs. I
    pinned them both to the same version.

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Provides NixOS configuration for the Raspberry Pi at my mum's place";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    my-nixos.url = "github:bjackman/nixos-flake?ref=master";
+  };
+
+  outputs = inputs@{ self, nixpkgs, ... }: {
+    nixosConfigurations.sandy = nixpkgs.lib.nixosSystem {
+      modules = [
+        inputs.my-nixos.nixosModules.brendan
+        ({ modulesPath, ... }: {
+          imports = [ "${modulesPath}/installer/sd-card/sd-image-aarch64.nix" ];
+
+          # Hmm, seems like cross-compilation is a bit of a mess, so here we just
+          # assume that this will always be built on a proper american computer
+          nixpkgs.buildPlatform = "x86_64-linux";
+          nixpkgs.hostPlatform = "aarch64-linux";
+          networking.hostName = "sandy";
+        })
+      ];
+    };
+  };
+}
