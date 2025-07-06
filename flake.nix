778ea519b95554739a29b44cd3c11e40109b17c6
Brendan Jackman <bhenryj0117@gmail.com>
2025-07-06 16:25:18 +0200

    Add and apply nixfmt-tree

--- flake.nix
+++ flake.nix
@@ -1,54 +1,64 @@
 {
-  description =
-    "Provides NixOS configuration for the Raspberry Pi at my mum's place";
+  description = "Provides NixOS configuration for the Raspberry Pi at my mum's place";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     my-nixos.url = "github:bjackman/nixos-flake?ref=master";
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }:
+  outputs =
+    inputs@{ self, nixpkgs, ... }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
       tailscaleAuthKeyFile = "/var/tmp/tailscale-auth-key"; # /var/tmp I guess...?
-      mkSandyConfig = hostName: nixpkgs.lib.nixosSystem {
-        modules = [
-          inputs.my-nixos.nixosModules.brendan
-          ({ modulesPath, ... }: {
-            imports = [
-              "${modulesPath}/installer/sd-card/sd-image-aarch64.nix"
-            ];
+      mkSandyConfig =
+        hostName:
+        nixpkgs.lib.nixosSystem {
+          modules = [
+            inputs.my-nixos.nixosModules.brendan
+            (
+              { modulesPath, ... }:
+              {
+                imports = [
+                  "${modulesPath}/installer/sd-card/sd-image-aarch64.nix"
+                ];
 
-            # Hmm, seems like cross-compilation is a bit of a mess, so here we just
-            # assume that this will always be built on a proper american computer
-            nixpkgs.buildPlatform = "x86_64-linux";
-            nixpkgs.hostPlatform = "aarch64-linux";
+                # Hmm, seems like cross-compilation is a bit of a mess, so here we just
+                # assume that this will always be built on a proper american computer
+                nixpkgs.buildPlatform = "x86_64-linux";
+                nixpkgs.hostPlatform = "aarch64-linux";
 
-            networking.hostName = hostName;
+                networking.hostName = hostName;
 
-            virtualisation.vmVariant.virtualisation = {
-              forwardPorts = [{
-                from = "host";
-                host.port = 2222;
-                guest.port = 22;
-              }];
-              graphics = false;
-              # Point to the x86 packages for running QEMU etc.
-              host.pkgs = pkgs;
-            };
+                virtualisation.vmVariant.virtualisation = {
+                  forwardPorts = [
+                    {
+                      from = "host";
+                      host.port = 2222;
+                      guest.port = 22;
+                    }
+                  ];
+                  graphics = false;
+                  # Point to the x86 packages for running QEMU etc.
+                  host.pkgs = pkgs;
+                };
 
-            system.stateVersion = "25.05";
+                system.stateVersion = "25.05";
 
-            # Note this requires running `sudo tailscale up` on the target to
-            # set up, unless you use the addTailscaleAuthKey mechanism.
-            services.tailscale = {
-              enable = true;
-              authKeyFile = tailscaleAuthKeyFile;
-            };
-          })
-        ];
-      };
-    in {
+                # Note this requires running `sudo tailscale up` on the target to
+                # set up, unless you use the addTailscaleAuthKey mechanism.
+                services.tailscale = {
+                  enable = true;
+                  authKeyFile = tailscaleAuthKeyFile;
+                };
+              }
+            )
+          ];
+        };
+    in
+    {
+      formatter.x86_64-linux = pkgs.nixfmt-tree;
+
       nixosConfigurations.sandy = mkSandyConfig "sandy";
       nixosConfigurations.norte = mkSandyConfig "norte";
 
@@ -66,25 +76,28 @@
         # it.
         addTailscaleAuthKey = pkgs.writeShellApplication {
           name = "add-tailscale-auth-key";
-          runtimeInputs = with pkgs; [ libguestfs-with-appliance zstd ];
+          runtimeInputs = with pkgs; [
+            libguestfs-with-appliance
+            zstd
+          ];
           text = ''
-          set -eux
+            set -eux
 
-          SOURCE_IMG_ZSTD="$1"
-          DEST_IMG="$2"
-          TS_AUTH_KEY_FILE="$3"
+            SOURCE_IMG_ZSTD="$1"
+            DEST_IMG="$2"
+            TS_AUTH_KEY_FILE="$3"
 
-          zstd -d "$SOURCE_IMG_ZSTD" -o "$DEST_IMG"
-          chmod +w out.img
+            zstd -d "$SOURCE_IMG_ZSTD" -o "$DEST_IMG"
+            chmod +w out.img
 
-          guestfish -a "$DEST_IMG" <<EOF
-            run
-            mount /dev/sda2 /
-            mkdir-p ${builtins.dirOf tailscaleAuthKeyFile}
-            upload $TS_AUTH_KEY_FILE ${tailscaleAuthKeyFile}
-            umount /
-            exit
-          EOF
+            guestfish -a "$DEST_IMG" <<EOF
+              run
+              mount /dev/sda2 /
+              mkdir-p ${builtins.dirOf tailscaleAuthKeyFile}
+              upload $TS_AUTH_KEY_FILE ${tailscaleAuthKeyFile}
+              umount /
+              exit
+            EOF
           '';
         };
       };
@@ -95,7 +108,11 @@
       };
 
       devShells.x86_64-linux.default = pkgs.mkShell {
-        packages = with pkgs; [ nixfmt-classic nixos-rebuild libguestfs-with-appliance ];
+        packages = with pkgs; [
+          nixfmt-classic
+          nixos-rebuild
+          libguestfs-with-appliance
+        ];
       };
     };
 }
